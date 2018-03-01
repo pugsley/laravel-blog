6e14f0f09559b841ff342caeddcc7bd571800deb
Tama <github@tama.co.nz>
2018-03-01 12:32:02 +0100

    Adding a few example controller tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,28 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
+use Mockery;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // Wrap all tests in a transaction so we're not polluting the db
+        DB::beginTransaction();
+    }
+
+    public function tearDown()
+    {
+        // If we're mocking objects with shouldReceive calls this
+        // will trigger Mockery to check the calls have or haven't been made
+        Mockery::close();
+        DB::rollBack();
+        parent::tearDown();
+    }
+
 }
